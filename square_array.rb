bfc526c22bcb3c7b8a39a04d4a0df8581beea408
Jake Zappin <jake.zappin@gmail.com>
2016-09-24 11:59:22 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  newarray = []
+
+  array.each do |number|
+    squarenum = number*number
+    newarray.push(squarenum)
+  end
+
+  return newarray
 end
